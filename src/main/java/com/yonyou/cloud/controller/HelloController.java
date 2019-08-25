596a3a5972d8cc2a5f1559fcf79ee116df610d6b
sirsoda <1563004690@qq.com>
2019-08-25 17:57:39 +0800

    --

--- src/main/java/com/yonyou/cloud/controller/HelloController.java
+++ src/main/java/com/yonyou/cloud/controller/HelloController.java
@@ -1,13 +1,15 @@
 package com.yonyou.cloud.controller;
 
+import com.yonyou.commons.feign.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-
-    @GetMapping("/hello")
-    public String hello(String name) {
-        return "hello " + name;
+public class HelloController implements HelloService {
+    @Override
+    public String hello(String s) {
+//        if(true)
+//            throw new RuntimeException();
+        return "hello " + s;
     }
 }
